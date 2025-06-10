75df73ce67cd891a5052eb900e291c7b8cf3c276
Arbi <arbihamolli111@gmail.com>
2025-06-10 23:57:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://secret-app-zwir.onrender.com/auth/google/secrets",
+    callbackURL: "https://secrets-g6b2.onrender.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
